14208c7d7a13905cf9b7de84235bf33b0e2e9ea3
jason brady <jwbwvf@gmail.com>
2016-09-03 13:07:31 -0500

    refactor to move hard coded data from views to controllers

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,15 +1,93 @@
 
 //GET locations page, home page
 module.exports.locations = function (req, res) {
-	res.render('locations', { title: 'Locations' });	
+	var locations = [
+		{
+			name: 'Starcups',
+			rating: 3,
+			distance: '100m',
+			address: '125 High Street, Reading RG6 1PS',
+			facilities: ['Hot drinks', 'Food', 'Premium wifi']
+		},
+				{
+			name: 'Cafe Hero',
+			rating: 4,
+			distance: '200m',
+			address: '125 Low Street, Reading RG9 2PS',
+			facilities: ['Hot drinks', 'Food', 'Premium wifi']
+		},
+				{
+			name: 'Burger Queen',
+			rating: 2,
+			distance: '250m',
+			address: '2 Fun Street, RG69 3PS',
+			facilities: ['Food', 'Premium wifi']
+		}
+	];
+
+	res.render('locations', { 
+		pageHeader: {
+			title: 'Loc8r',
+			strapLine: 'Find places to work with wifi near you!' 
+		},
+		locations: locations,
+		sidebar: "Looking for wifi and a seat? Loc8r helps you find places"
+		 + "to work when you and about.Perhaps with coffee, cake or a pint?"
+		 + "Let Loc8r help you find the place you're looking for."
+	});
 };
 
 //GET location page
 module.exports.location = function (req, res) {
-	res.render('location', { title: 'Location' });	
+	var map = {
+		coordinates: {lat: 51.455041, lng: -0.9690884},
+		zoom: 17,
+		size: '400x350',
+		sensor: false,
+		scale: 2
+	}
+
+	var reviews = [
+		{
+			rating: 5,
+			author: 'Jason Brady',
+			date: '31 August 2016',
+			comment: "What a great place. I can't say enough good things about it."
+		},
+		{
+			rating: 3,
+			author: 'Amanda Brady',
+			date: '19 June 2016',
+			comment: "It was okay. Coffee wasn't great, but the wifi was fast."
+		},
+	];
+	var location = {
+		name: 'Starcups',
+		rating: 3,
+		distance: '100m',
+		address: '125 High Street, Reading RG6 1PS',
+		facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+		hours: ['Monday - Friday: 7:00am - 7:00pm', 'Saturday : 8:00am - 5:00pm', 'Sunday : closed'],
+		reviews: reviews,
+		map: map
+	};
+
+	res.render('location', {
+		pageHeader: {
+			title: 'Starcups'
+		},
+		location: location,
+		sidebar: {
+			context: "Starcups cafe is on Loc8r because it has accessible wifi and space to.sit down with your laptop and get some work done.",
+			callToAction: "If you've been and you like it - or if you don't - please leave a review to help other people just like you."
+		}
+	});	
 };
 
 //GET add review page
 module.exports.newReview = function (req, res) {
-	res.render('newReview', { title: 'New Review' });	
+	res.render('newReview', { 
+		title: 'Review Starcups on Loc8r',
+		pageHeader: {title: 'Review Starcups'}
+	});	
 };
--- app_server/controllers/others.js
+++ app_server/controllers/others.js
@@ -1,5 +1,9 @@
 
 //GET home page
 module.exports.about = function (req, res) {
-	res.render('about', { title: 'About' });	
+	res.render('about', { 
+		title: 'About Loc8r',
+		content: "Loc8r was created to help people find places to sit down and get a bit of work done.\n\n"
+          + "Lorem ipsum dolo sit amet, consectetur adipiscing elit.  Nunc sed lorem ac nisi dignissim accumsan."
+	});	
 };
